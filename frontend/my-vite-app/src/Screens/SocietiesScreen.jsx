7263fed80b44cfe1553440257bc51cac580c4c0f
moazakramkhan1 <moazkhan2002@gmail.com>
2025-03-21 16:08:55 +0500

    added redux

--- frontend/my-vite-app/src/Screens/SocietiesScreen.jsx
+++ frontend/my-vite-app/src/Screens/SocietiesScreen.jsx
@@ -1,16 +1,24 @@
 import SocietyComponent from "../components/SocietyComponent"
 import SideNavbar from "../components/SideNavbar"
 import '../styles/SocietiesScreen.css'
+import { useSelector } from 'react-redux'
 function SocietiesScreen() {
-    return (
-        <div className="societies-screen">
-          <SideNavbar />
-          <div className="content">
-            <h1 className="page-title">Societies</h1>
-            <SocietyComponent />
-          </div>
-        </div>
-      );
+  const user = useSelector((state) => state.user.user);
+  return (
+    <div className="societies-screen">
+      <SideNavbar />
+      <div className="content">
+        <h1 className="page-title">Societies</h1>
+        {user?.role === 'admin' &&
+          <div className="admin-actions">
+            <button className="createSociety-btn">Create Society </button>
+            <button className="editSociety-btn">Edit Society </button>
+            <button className="DeleteSociety-btn">Delete Society </button>
+          </div>}
+        <SocietyComponent />
+      </div>
+    </div>
+  );
 }
 
 export default SocietiesScreen;
