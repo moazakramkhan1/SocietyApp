59897a4b3bc1609a8fe078e4efbb7a529567070d
MuhammadAwaisGhaffar <awaisghaffar112@gmail.com>
2025-03-27 21:40:10 +0500

    Delete Society Functionality

--- frontend/my-vite-app/src/Screens/SocietyDetailsScreen.jsx
+++ frontend/my-vite-app/src/Screens/SocietyDetailsScreen.jsx
@@ -6,18 +6,50 @@ import getRoleORImageOREmailORId from "../getRole";
 import '../styles/SocietyDetailScreen.css'
 import Modal from "../components/Modal";
 import UpdateSocietyFormComponent from "../components/UpdateSocietyFormComponent";
+import { useNavigate } from "react-router-dom";
+import { DeleteSocietyURL } from "../endPointUrls";
+import {SocietiesScreenRoute} from '../routes';
+import axios from "axios";
+
+
 
 const SocietyDetailsScreen = () => {
   const userRole = getRoleORImageOREmailORId(1);
   const user_id = getRoleORImageOREmailORId(4);
   const [modalStatus, setModalStatus] = useState(false);
   const [societyData, setSocietyData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
   const openModal = () => {
     setModalStatus(true);
   }
   const closeModal = () => {
     setModalStatus(false);
   }
+  const handleDeleteSociety = async () => {
+    if (!societyData?.id) {
+      alert("Society ID is missing!");
+      return;
+    }
+
+    const confirmDelete = window.confirm(
+      "Are you sure you want to delete this society? This action cannot be undone."
+    );
+
+    if (!confirmDelete) return;
+
+    try {
+      setLoading(true);
+      await axios.delete(`${DeleteSocietyURL}${societyData.id}`);
+      alert("Society deleted successfully!");
+      navigate(SocietiesScreenRoute); 
+    } catch (error) {
+      console.error("Error deleting society:", error);
+      alert("Failed to delete society. Please try again.");
+    } finally {
+      setLoading(false);
+    }
+  };
 
 
   return (
@@ -28,7 +60,9 @@ const SocietyDetailsScreen = () => {
         {(userRole == 'admin' && user_id === societyData?.admin_id) &&
           <div className="admin-actions">
             <button className="editSociety-btn" onClick={openModal}>Edit Society </button>
-            <button className="DeleteSociety-btn">Delete Society </button>
+            <button className="DeleteSociety-btn" onClick={handleDeleteSociety} disabled = {loading}>Delete Society
+               {loading ? "Deleting..." : "Delete Society"}
+            </button>
           </div>
         }
         <SocietyDetails setSocietyData={setSocietyData} setLoading={setLoading} />
